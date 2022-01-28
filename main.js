26e2059788ab57fe29d7173846b7e84e62bb239c
DMITRY PRAKHT <98157604+Prakht-Dm@users.noreply.github.com>
2022-01-28 23:51:44 +0700

    Added "\n \," in console.log

--- main.js
+++ main.js
@@ -15,23 +15,18 @@ function showList(){
     let createDone="";
     for (let key in list){
         switch (list[key]){
-            case "To Do" : createTodo+=(key+",\n");
+            case "To Do" : (createTodo === "") ? (createTodo+=(`\"${key}\"`)) : (createTodo+=(`,\n\"${key}\"`));
             break;
-            case "In Progress" : createInProgress+=(key+",\n");
+            case "In Progress" : (createInProgress === "") ? (createInProgress+=(`\"${key}\"`)) : (createInProgress+=(`,\n\"${key}\"`));
             break;
-            case "Done" : createDone+=(key+",\n");
+            case "Done" : (createDone === "") ? (createDone+=(`\"${key}\"`)) : (createDone+=(`,\n\"${key}\"`));
             break;
         }
     }
     if (createTodo === "") createTodo = "-\n";
     if (createInProgress === "") createInProgress = "-\n";
     if (createDone ==="") createDone = "-\n"
-    console.log(`Todo:\n
-    ${createTodo}
-    In Progress:\n
-    ${createInProgress}
-    Done:\n
-    ${createDone}`)
+    console.log(`Todo:\n${createTodo}\nIn Progress:\n${createInProgress}\nDone:\n${createDone}`)
 }
 addTask("Wake up");
 addTask("Have breakfast");
@@ -42,5 +37,3 @@ changeStatus("Wake up","Done");
 changeStatus("have breakfast","Done");
 changeStatus("Go to work","In Progress");
 showList();
-
-
