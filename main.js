bfb9a1466937f48b3a87621d7b7223228c0f5789
DMITRY PRAKHT <98157604+Prakht-Dm@users.noreply.github.com>
2022-02-05 23:14:59 +0700

    TODO made on Arrs

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-const list = {};
-function changeStatus(task,status){
-    list[task] = status;
+const list = [];
+const STATUS_NAME = {
+    TODO: "To Do",
+    INPROGRESS: "In Progress",
+    DONE: "Done",
 }
-function addTask(newTask){
-    list[newTask] = "To Do";
+const PRIORITY_LIST={
+    HIGH: "High",
+    MEDIUM: "Medium",
+    LOW: "Low",
+}
+function changeStatus(task,stat){
+    for (let i=0; i<list.length;i++){
+        if (list[i].name===task){
+        list[i].status=stat;
+        break;
+    } 
+ }
+}
+function addTask(Task){
+    list.push({name: Task,
+    status: STATUS_NAME.TODO,
+    priority: PRIORITY_LIST.MEDIUM,});
 }
 function deleteTask(task){
-    delete list[task];
+       for (let i=0; i<list.length;i++){
+           if (list[i].name===task){
+           list.splice(i,1);
+           break;
+       }
+    }
 }
 function showList(){
     let createTodo ="",
-        createInProgress ="",
-        createDone="";
-    for (let key in list){
-        switch (list[key]){
-            case "To Do" : createTodo += (createTodo === "") ? `\"${key}\"` : `,\n\"${key}\"`;
-            break;
-            case "In Progress" : createInProgress += (createInProgress === "") ? `\"${key}\"` : `,\n\"${key}\"`;
-            break;
-            case "Done" : createDone += (createDone === "") ? `\"${key}\"` : `,\n\"${key}\"`;
-            break;
-        }
+    createInProgress ="",
+    createDone="";
+    for (let i=0;i<list.length;i++){
+        if (list[i].status===STATUS_NAME.TODO) {createTodo+=`${list[i].name}\n`;}
+        if (list[i].status===STATUS_NAME.INPROGRESS) createInProgress+=`${list[i].name}\n`;
+        if (list[i].status===STATUS_NAME.DONE) createDone+=`${list[i].name}\n`;
     }
     console.log(`Todo:\n${createTodo||`-`}\nIn Progress:\n${createInProgress||`-`}\nDone:\n${createDone||`-`}`)
 }
@@ -29,6 +46,6 @@ addTask("Have breakfast");
 addTask("Go to work");
 addTask("Make money");
 addTask("Learn Javascript");
-changeStatus("Wake up","Done");
-changeStatus("have breakfast","Done");
+changeStatus("Wake up",STATUS_NAME.INPROGRESS);
+changeStatus("Have breakfast","Done");
 showList();
